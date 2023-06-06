63a26393d127e457fb356ecd994a1dd6b92f8cfb
zubiik97 <122887381+zubiik97@users.noreply.github.com>
2023-06-06 13:25:30 +0000

    Deploying to gh-pages from @ zubiik97/goit-js-hw-09@5e11ab595aeb931e085939b956b5be0d65f0dfb0 🚀

---
+++ 01-color-switcher.ef210bed.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body");let r=null;function n(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){r=setInterval(n,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.ef210bed.js.map
