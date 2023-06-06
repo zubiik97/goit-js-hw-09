5e11ab595aeb931e085939b956b5be0d65f0dfb0
zubiik97 <zubiik97@gmail.com>
2023-06-06 16:20:21 +0300

    06.06

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const dataStartRef = document.querySelector('[data-start]');
+const dataStopRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+
+let timerId = null;
+
+dataStartRef.addEventListener('click', onStart);
+dataStopRef.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(getBgColor, 1000);
+
+  dataStartRef.toggleAttribute('disabled');
+}
+
+// Function stop event listener
+function onStop() {
+  clearInterval(timerId);
+
+  dataStartRef.removeAttribute('disabled');
+}
+
+// Function set random bg color
+function getBgColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,117 @@
+// Import library
+import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Import additional css styles
+import 'flatpickr/dist/flatpickr.min.css';
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+
+// Get element date input, start btn, data: days, hours, min, sec
+let getRef = selector => document.querySelector(selector);
+const imputDatePickerRef = getRef('#datetime-picker');
+const btnStartRef = getRef('[data-start]');
+const daysRef = getRef('[data-days]');
+const hoursRef = getRef('[data-hours]');
+const minutesRef = getRef('[data-minutes]');
+const secondsRef = getRef('[data-seconds]');
+
+// Set initial value
+let timeDifference = 0;
+let timerId = null;
+let formatDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    currentDifferenceDate(selectedDates[0]);
+  },
+};
+
+btnStartRef.setAttribute('disabled', true);
+
+// Initial flatpickr
+flatpickr(imputDatePickerRef, options);
+
+// Set click event listener on button start
+btnStartRef.addEventListener('click', onBtnStart);
+// Reset timer on btn
+window.addEventListener('keydown', e => {
+  if (e.code === 'Escape' && timerId) {
+    clearInterval(timerId);
+
+    imputDatePickerRef.removeAttribute('disabled');
+    btnStartRef.setAttribute('disabled', true);
+
+    secondsRef.textContent = '00';
+    minutesRef.textContent = '00';
+    hoursRef.textContent = '00';
+    daysRef.textContent = '00';
+  }
+});
+
+// Start timer
+function onBtnStart() {
+  timerId = setInterval(startTimer, 1000);
+}
+
+//date checking and rendering of date difference
+function currentDifferenceDate(selectedDates) {
+  const currentDate = Date.now();
+
+  if (selectedDates < currentDate) {
+    btnStartRef.setAttribute('disabled', true);
+    return Notify.failure('Please choose a date in the future');
+  }
+
+  timeDifference = selectedDates.getTime() - currentDate;
+  formatDate = convertMs(timeDifference);
+
+  renderDate(formatDate);
+  btnStartRef.removeAttribute('disabled');
+}
+
+//Timer
+function startTimer() {
+  btnStartRef.setAttribute('disabled', true);
+  imputDatePickerRef.setAttribute('disabled', true);
+
+  timeDifference -= 1000;
+
+  if (secondsRef.textContent <= 0 && minutesRef.textContent <= 0) {
+    Notify.success('Time end');
+    clearInterval(timerId);
+  } else {
+    formatDate = convertMs(timeDifference);
+    renderDate(formatDate);
+  }
+}
+
+// Rendering date
+function renderDate(formatDate) {
+  secondsRef.textContent = formatDate.seconds;
+  minutesRef.textContent = formatDate.minutes;
+  hoursRef.textContent = formatDate.hours;
+  daysRef.textContent = formatDate.days;
+}
